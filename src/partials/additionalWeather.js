74eb0d724f83dd18a9511df766309be774aaf31c
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-08-15 21:44:17 +0300

    errors

--- src/partials/additionalWeather.js
+++ src/partials/additionalWeather.js
@@ -1,7 +1,6 @@
 import moment from 'moment-timezone'; // Biblioteca pt manipularea timpului.
 import { getWeatherByCityName } from '../apiOpenWeather.js';
 import { getTimeZoneByCoordinates } from '../timezoneApi.js';
-import 'animate.css';
 
 export async function fetchAdditionalWeatherData(city) {
   if (!city) {
